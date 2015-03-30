47aa7ccd89ee0b59865dddefdafb8a207a5fe20b
Yuki Ahmed <aaron.ahmed.eve@gmail.com>
2015-03-31 05:04:17 +0530

    support for screen_name logins

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,22 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => { email: true, login: false }
+
+  attr_accessor :login
+
+  validates :screen_name,
+    :presence => true,
+    :uniqueness => {
+      :case_sensitive => false
+    }
+
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_h).where(["lower(screen_name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      else
+        where(conditions.to_h).first
+      end
+    end
 end
