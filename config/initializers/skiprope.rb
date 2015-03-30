5d6947e09bed89e4edc53ddf937efebebaded203
Yuki Ahmed <aaron.ahmed.eve@gmail.com>
2015-03-30 09:52:59 +0530

    Rails.application.config.skiprope => , GIT_REV and GIT_CONTIRBUTORS values

--- config/initializers/skiprope.rb
+++ config/initializers/skiprope.rb
@@ -1,1 +1,7 @@
-Rails.application.config.skiprope = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+$skiprope = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+
+$skiprope["GIT_REV"] = `git rev-parse --short HEAD`
+
+$skiprope["GIT_CONTRIBUTORS"] = `git shortlog -ens --after="2015-03-27 21:40:10"`.scan /\t(.*)\s<(.*)>$/m
+
+Rails.application.config.skiprope = $skiprope
